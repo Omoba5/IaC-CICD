e6687abb96cc4c4e5d10c64ff9d7f79eed6e6667
Omoba <ogabiprince@gmail.com>
2023-07-26 14:48:28 +0100

    fixed credentials

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Authenticate with GCP Service account
 provider "google" {
-  credentials = file("./CREDENTIAL")
+  credentials = file("${CREDENTIAL}")
 
   project = "infrastructure-393911"
   region  = "us-central1"
