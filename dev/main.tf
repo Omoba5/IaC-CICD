1f83017a3150681bb0668e6d2b1b5c9c771264cc
Omoba <ogabiprince@gmail.com>
2023-07-28 10:08:04 +0100

    Fixed module path

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,6 @@
 # Use the debian module to provision the prod environment
 module "debian_vm" {
-  source = "./modules/debian_vm"
+  source = "../modules/debian_vm"
   
   # Input variables
   environment = "prod"
